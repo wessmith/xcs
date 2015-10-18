131fa524b94482078ad78da1cf3c5da01f6db386
Wes Smith <wes@w5mith.com>
2015-10-17 21:20:45 -0400

    enable code coverage

---
+++ xcs/SomeObject.m
@@ -0,0 +1,25 @@
+//
+//  SomeObject.m
+//  xcs
+//
+//  Created by Wes on 10/17/15.
+//
+//
+
+#import "SomeObject.h"
+
+@implementation SomeObject
+
++ (NSInteger)sumOfNumber:(NSInteger)number adding:(NSInteger)add {
+    return number + add;
+}
+
++ (NSInteger)resultOfNumber:(NSInteger)number subtracting:(NSInteger)subtract {
+    return number - subtract;
+}
+
++ (NSInteger)resultOfNumber:(NSInteger)number multipliedBy:(NSInteger)multiplier {
+    return number * multiplier;
+}
+
+@end
--- xcsTests/xcsTests.m
+++ xcsTests/xcsTests.m
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "SomeObject.h"
 
 @interface xcsTests : XCTestCase
 
@@ -35,6 +36,18 @@
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
+}
+
+
+- (void)testAddition {
+    NSInteger result = [SomeObject sumOfNumber:2 adding:2];
+    XCTAssertEqual(result, 4);
+}
+
+
+- (void)testSubtraction {
+    NSInteger result = [SomeObject resultOfNumber:2 subtracting:1];
+    XCTAssertEqual(result, 1);
 }
 
 @end
